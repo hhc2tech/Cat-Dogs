180de8bf51efa7307545e2f04d06f318177f63a9
jiangxubin <21612149@zju.edu.cn>
2018-06-13 22:23:47 +0800

    Finish feature vector export part

--- load_data.py
+++ load_data.py
@@ -7,11 +7,9 @@ def split_train_dataset(path):
     Search through the directory of train data and split all files into cat and train set
     :return: Cat set Dog set
     """
-    cat_train = None
-    dog_train = None
-    for root, dirs, files in os.walk(path):
-        cat_train = filter(lambda x: x[:3] == 'cat', files)
-        dog_train = filter(lambda x: x[:3] == 'dog', files)
+    files = os.listdir(path)
+    cat_train = filter(lambda x: x[:3] == "cat", files)
+    dog_train = filter(lambda x: x[:3] == 'dog', files)
     return cat_train, dog_train
 
 
@@ -27,11 +25,13 @@ def rm_mkdir(dirname):
 
 
 if __name__ == "__main__":
-    cat_train, dog_train = split_train_dataset(r'G:\Kaggle\Cat-Dogs\train')
-    rm_mkdir('data/train1')
-    os.mkdir('data/train1/cat')
-    os.mkdir('data/train1/dog')
-    rm_mkdir('data/test1')
-    os.symlink('data/test', 'data/test1')
-    [os.symlink('data/train' + file, 'data/train1/cat' + file) for file in cat_train]
-    [os.symlink('data/train' + file, 'data/train1/dog' + file) for file in dog_train]
+    cat_train, dog_train = split_train_dataset(r'G:\Kaggle\Cat-Dogs\data\train')
+    rm_mkdir('G:\Kaggle\Cat-Dogs\data/train1')
+    os.mkdir('G:\Kaggle\Cat-Dogs\data/train1/cat')
+    os.mkdir('G:\Kaggle\Cat-Dogs\data/train1/dog')
+    rm_mkdir('G:\Kaggle\Cat-Dogs\data/test1')
+    os.symlink('G:/Kaggle/Cat-Dogs/data/test', 'G:/Kaggle/Cat-Dogs/data/test1/test')
+    [os.symlink('G:\Kaggle/Cat-Dogs/data/train/' + file, 'G:/Kaggle/Cat-Dogs/data/train1/cat/' + file) for file in cat_train]
+    [os.symlink('G:/Kaggle/Cat-Dogs/data/train/' + file, 'G:/Kaggle/Cat-Dogs/data/train1/dog/' + file) for file in dog_train]
+    # IF OSError: symbolic link privilege not held occurred, use windows ipython under the previlidge of administer
+    # https: // github.com / explosion / spaCy / issues / 895
